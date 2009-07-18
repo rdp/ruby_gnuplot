899ff16beb1b41042783942a69e70b545d8482b4
rogerdpack <rogerpack2005@gmail.com>
2009-07-18 15:54:35 -0600

    better error message

--- lib/gnuplot.rb
+++ lib/gnuplot.rb
@@ -56,7 +56,7 @@ module Gnuplot
   # Return the path to the gnuplot executable or nil if one cannot be found.
   def Gnuplot.gnuplot( persist = true )
     cmd = which( ENV['RB_GNUPLOT'] || 'gnuplot' )
-    raise 'gnuplot not found' unless cmd
+    raise 'gnuplot executable not found' unless cmd
     cmd += " -persist" if persist
     cmd
   end
