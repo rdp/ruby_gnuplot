a1413b52ed3e302496bd8d3bd38e8445766bf44d
Bernard Lambeau <blambeau@gmail.com>
2011-01-26 11:27:12 +0100

    Added an example on how to output to an image file

---
+++ examples/output_image_file.rb
@@ -0,0 +1,32 @@
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+require "gnuplot"
+
+# See sin_wave.rb first
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new( gp ) do |plot|
+  
+    # The following lines allow outputting the graph to an image file. 
+    # The first set the kind of image that you want, while the second
+    # redirects the output to a given file. 
+    #
+    # Typical terminals: gif, png, postscript, latex, texdraw
+    #
+    # See http://mibai.tec.u-ryukyu.ac.jp/~oshiro/Doc/gnuplot_primer/gptermcmp.html
+    # for a list of recognized terminals.
+    #
+    plot.terminal "gif"
+    plot.output   File.expand_path("../sin_wave.gif", __FILE__)
+  
+    # see sin_wave.rb
+    plot.xrange "[-10:10]"
+    plot.title  "Sin Wave Example"
+    plot.ylabel "x"
+    plot.xlabel "sin(x)"
+    
+    plot.data << Gnuplot::DataSet.new( "sin(x)" ) do |ds|
+      ds.with = "lines"
+      ds.linewidth = 4
+    end
+    
+  end
+end
