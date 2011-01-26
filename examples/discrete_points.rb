a6b0a6109d7988f1176ad0ccb1316b9ea75ed994
Bernard Lambeau <blambeau@gmail.com>
2011-01-26 11:05:55 +0100

    Added examples from README in examples/ folder

---
+++ examples/discrete_points.rb
@@ -0,0 +1,19 @@
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+require "gnuplot"
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new( gp ) do |plot|
+  
+    plot.title  "Array Plot Example"
+    plot.ylabel "x"
+    plot.xlabel "x^2"
+    
+    x = (0..50).collect { |v| v.to_f }
+    y = x.collect { |v| v ** 2 }
+
+    plot.data << Gnuplot::DataSet.new( [x, y] ) do |ds|
+      ds.with = "linespoints"
+      ds.notitle
+    end
+
+  end
+end
