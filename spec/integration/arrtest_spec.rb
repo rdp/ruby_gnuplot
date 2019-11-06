8e42b9082f8a709031e9e80b7c4e682c544ea158
darthjee <darthjee@gmail.com>
2019-11-06 22:04:00 +0100

    Add arrtest_spec

---
+++ spec/integration/arrtest_spec.rb
@@ -0,0 +1,39 @@
+describe "Array Plot Example" do
+  let(:file_path)    { 'spec/tmp/array_plot.eps' }
+  let(:fixture_path) { 'spec/fixtures/plots/array_plot.eps' }
+
+  let(:file_content)    { File.read(file_path) }
+  let(:fixture_content) { File.read(fixture_path) }
+
+  before do
+    path = file_path
+
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new( gp ) do |plot|
+
+        plot.title  "Array Plot Example"
+        plot.ylabel "x"
+        plot.xlabel "x^2"
+        plot.set "term","postscript eps"
+        plot.set "out", "'#{path}'"
+
+        x = (0..50).collect { |v| v.to_f }
+        y = x.collect { |v| v ** 2 }
+
+        plot.data << Gnuplot::DataSet.new( [x, y] ) do |ds|
+          ds.with = "linespoints"
+          ds.notitle
+        end
+      end
+    end
+
+  end
+
+  after do
+    File.delete(file_path)
+  end
+
+  it do
+    expect(file_content).to eq(fixture_content)
+  end
+end
