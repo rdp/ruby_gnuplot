7c6ebdf2f44f6abbb7f96a39b88e0f7bc0fc286f
darthjee <darthjee@gmail.com>
2019-11-22 22:15:17 +0100

    Replace whitespaces

--- test/histtest.rb
+++ test/histtest.rb
@@ -1,20 +1,20 @@
 require 'gnuplot'
 
 Gnuplot.open do |gp|
-    gp << "bin(x, s) = s*int(x/s)\n"
+  gp << "bin(x, s) = s*int(x/s)\n"
 
-    Gnuplot::Plot.new( gp ) do |plot|
-	plot.title  "Histogram"
-	plot.xlabel "x"
-	plot.ylabel "frequency"
+  Gnuplot::Plot.new( gp ) do |plot|
+    plot.title  "Histogram"
+    plot.xlabel "x"
+    plot.ylabel "frequency"
 
-	x = (0..500).collect { |v| (rand()-0.5)**3 }
-	plot.data << Gnuplot::DataSet.new( [x] ) do |ds|
-	    ds.title = "smooth frequency"
-	    ds.using = "(bin($1,.01)):(1.)"
-	    ds.smooth = "freq"
-	    ds.with = "boxes"
-	end
+    x = (0..500).collect { |v| (rand()-0.5)**3 }
+    plot.data << Gnuplot::DataSet.new( [x] ) do |ds|
+      ds.title = "smooth frequency"
+      ds.using = "(bin($1,.01)):(1.)"
+      ds.smooth = "freq"
+      ds.with = "boxes"
     end
+  end
 end
 
